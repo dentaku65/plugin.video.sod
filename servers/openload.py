edc0a23da6031a7dad852826717377295edd7375
Dentaku65 <dentaku65@gmail.com>
2016-06-21 20:15:28 +0200

    servers

---
+++ servers/openload.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# streamondemand - XBMC Plugin
+# Conector for openload.co
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+# ------------------------------------------------------------
+
+# fixed by cmos
+
+import re
+
+from core import logger
+from core import scrapertools
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'}
+
+
+def test_video_exists(page_url):
+    logger.info("streamondemand.servers.openload test_video_exists(page_url='%s')" % page_url)
+
+    data = scrapertools.downloadpageWithoutCookies(page_url)
+
+    if 'We are sorry!' in data:
+        return False, "[Openload] Il file è stato cancellato" 
+
+    return True, ""
+
+
+def get_video_url(page_url, premium=False, user="", password="", video_password=""):
+    logger.info("streamondemand.servers.openload url=" + page_url)
+    video_urls = []
+
+    data = scrapertools.downloadpageWithoutCookies(page_url)
+    subtitle = scrapertools.find_single_match(data, '<track kind="captions" src="([^"]+)" srclang="es"')
+    #Header para la descarga
+    header_down = "|User-Agent="+headers['User-Agent']+"|"
+
+    if "videocontainer" not in data:
+        url = page_url.replace("/embed/","/f/")
+        data = scrapertools.downloadpageWithoutCookies(url)
+        text_encode = scrapertools.find_single_match(data,"Click to start Download.*?<script[^>]+>(.*?)</script")
+        text_decode = decode(text_encode)
+
+        videourl = scrapertools.find_single_match(text_decode, '(http.*?)\}')
+        videourl = videourl.replace("https://","http://")
+        extension = videourl[-4:]
+        video_urls.append([ extension + " [Openload]", videourl+header_down+extension])
+    else:
+        text_encode = scrapertools.find_multiple_matches(data,'<script type="text/javascript">(ﾟωﾟ.*?)</script>')
+        # Buscamos la variable que nos indica el script correcto
+        subtract = scrapertools.find_single_match(data, 'welikekodi_ya_rly = ([^;]+)')
+        index = eval(subtract)
+        text_decode = decode(text_encode[index])
+        videourl = scrapertools.find_single_match(text_decode, "(http.*?true)")
+     
+        videourl = scrapertools.get_header_from_response(videourl, header_to_get="location")
+        videourl = videourl.replace("https://","http://").replace("?mime=true","")
+        extension = videourl[-4:]
+        video_urls.append([extension + " [Openload] ", videourl+header_down+extension, 0, subtitle])
+
+    for video_url in video_urls:
+        logger.info("streamondemand.servers.openload %s - %s" % (video_url[0],video_url[1]))
+
+    return video_urls
+
+
+# Encuentra vídeos del servidor en el texto pasado
+def find_videos(text):
+    encontrados = set()
+    devuelve = []
+
+    patronvideos = '//(?:www.)?openload.../(?:embed|f)/([0-9a-zA-Z-_]+)'
+    logger.info("streamondemand.servers.openload find_videos #" + patronvideos + "#")
+
+    matches = re.compile(patronvideos, re.DOTALL).findall(text)
+
+    for media_id in matches:
+        titulo = "[Openload]"
+        url = 'https://openload.co/embed/%s/' % media_id
+        if url not in encontrados:
+            logger.info("  url=" + url)
+            devuelve.append([titulo, url, 'openload'])
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada=" + url)
+
+    return devuelve
+
+
+def decode(text):
+    text = re.sub(r"\s+", "", text)
+    data = text.split("+(ﾟДﾟ)[ﾟoﾟ]")[1]
+    chars = data.split("+(ﾟДﾟ)[ﾟεﾟ]+")[1:]
+
+    txt = ""
+    for char in chars:
+        char = char \
+            .replace("(oﾟｰﾟo)","u") \
+            .replace("c", "0") \
+            .replace("(ﾟДﾟ)['0']", "c") \
+            .replace("ﾟΘﾟ", "1") \
+            .replace("!+[]", "1") \
+            .replace("-~", "1+") \
+            .replace("o", "3") \
+            .replace("_", "3") \
+            .replace("ﾟｰﾟ", "4") \
+            .replace("(+", "(")
+        char = re.sub(r'\((\d)\)', r'\1', char)
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\+\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\^\d\^\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\+\d\+\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\+\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        for x in scrapertools.find_multiple_matches(char,'(\(\d\-\d\))'):
+            char = char.replace( x, str(eval(x)) )
+        if 'u' not in char: txt+= char + "|"
+    txt = txt[:-1].replace('+','')
+    txt_result = "".join([ chr(int(n, 8)) for n in txt.split('|') ])
+    sum_base = ""
+    m3 = False
+    if ".toString(" in txt_result:
+        if "+(" in  txt_result:
+            m3 = True
+            sum_base = "+"+scrapertools.find_single_match(txt_result,".toString...(\d+).")
+            txt_pre_temp = scrapertools.find_multiple_matches(txt_result,"..(\d),(\d+).")
+            txt_temp = [ (n, b) for b ,n in txt_pre_temp ]
+        else:
+            txt_temp = scrapertools.find_multiple_matches(txt_result, '(\d+)\.0.\w+.([^\)]+).')
+        for numero, base in txt_temp:
+            code = toString( int(numero), eval(base+sum_base) )
+            if m3:
+                txt_result = re.sub( r'"|\+', '', txt_result.replace("("+base+","+numero+")", code) )
+            else:
+                txt_result = re.sub( r"'|\+", '', txt_result.replace(numero+".0.toString("+base+")", code) )
+    return txt_result
+
+
+def toString(number,base):
+   string = "0123456789abcdefghijklmnopqrstuvwxyz"
+   if number < base:
+      return string[number]
+   else:
+      return toString(number//base,base) + string[number%base]
+
