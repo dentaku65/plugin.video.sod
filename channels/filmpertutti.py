5eb1a3f58981b40d786110d4d2e133965bbe43ff
Dr-Zer0 <Dr-Zer0@suremail.info>
2016-07-07 00:17:21 +0200

    fix library

--- channels/filmpertutti.py
+++ channels/filmpertutti.py
@@ -34,16 +34,19 @@ def mainlist(item):
     itemlist = [Item(channel=__channel__,
                      title="[COLOR azure]Ultimi film inseriti[/COLOR]",
                      action="peliculas",
+                     extra="film",
                      url="%s/category/film/news_film/" % host,
                      thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
                 Item(channel=__channel__,
                      title="[COLOR azure]Categorie film[/COLOR]",
                      action="categorias",
+                     extra="film",
                      url="%s/category/film/" % host,
                      thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"),
                 Item(channel=__channel__,
                      title="[COLOR yellow]Cerca...[/COLOR]",
                      action="search",
+                     extra="film",
                      thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"),
                 Item(channel=__channel__,
                      title="[COLOR azure]Serie TV[/COLOR]",
@@ -96,13 +99,14 @@ def peliculas(item):
             "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
         itemlist.append(infoSod(
             Item(channel=__channel__,
-                 action="episodios" if item.extra == "serie" else "findvideos",
+                 action="findvideos",
                  fulltitle=scrapedtitle,
                  show=scrapedtitle,
                  title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
                  url=scrapedurl,
                  thumbnail=scrapedthumbnail,
                  plot=scrapedplot,
+                 extra=item.extra,
                  folder=True), tipo='movie'))
 
     # Extrae el paginador
@@ -122,6 +126,7 @@ def peliculas(item):
                  title="[COLOR orange]Successivo >>[/COLOR]",
                  url=scrapedurl,
                  thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 extra=item.extra,
                  folder=True))
 
     return itemlist
@@ -148,13 +153,14 @@ def peliculas_tv(item):
             "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
         itemlist.append(infoSod(
             Item(channel=__channel__,
-                 action="episodios" if item.extra == "serie" else "findvideos",
+                 action="episodios",
                  fulltitle=scrapedtitle,
                  show=scrapedtitle,
                  title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
                  url=scrapedurl,
                  thumbnail=scrapedthumbnail,
                  plot=scrapedplot,
+                 extra=item.extra,
                  folder=True), tipo='tv'))
 
     # Extrae el paginador
@@ -174,6 +180,7 @@ def peliculas_tv(item):
                  title="[COLOR orange]Successivo >>[/COLOR]",
                  url=scrapedurl,
                  thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 extra=item.extra,
                  folder=True))
 
     return itemlist
@@ -205,13 +212,14 @@ def anime(item):
             "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
         itemlist.append(
             Item(channel=__channel__,
-                 action="episodianime",
+                 action="episodios",
                  fulltitle=scrapedtitle,
                  show=scrapedtitle,
                  title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
                  url=scrapedurl,
                  thumbnail=scrapedthumbnail,
                  plot=scrapedplot,
+                 extra=item.extra,
                  folder=True))
 
     # Extrae el paginador
@@ -231,6 +239,7 @@ def anime(item):
                  title="[COLOR orange]Successivo >>[/COLOR]",
                  url=scrapedurl,
                  thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 extra=item.extra,
                  folder=True))
 
     return itemlist
@@ -267,6 +276,7 @@ def categorias(item):
                  title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
                  url=scrapedurl,
                  thumbnail=scrapedthumbnail,
+                 extra=item.extra,
                  plot=scrapedplot))
 
     return itemlist
@@ -276,12 +286,12 @@ def search(item, texto):
     logger.info("streamondemand.filmpertutti " + item.url + " search " + texto)
     item.url = host + "/search/" + texto
     try:
+        if item.extra == "film":
+            return peliculas(item)
         if item.extra == "serie":
             return peliculas_tv(item)
         if item.extra == "anime":
             return anime(item)
-        else:
-            return peliculas(item)
     # Se captura la excepción, para no interrumpir al buscador global si un canal falla
     except:
         import sys
@@ -290,8 +300,30 @@ def search(item, texto):
         return []
 
 
+def episodios(item):
+    logger.info("streamondemand.filmpertutti episodios")
+
+    itemlist = episodiserie(item) if item.extra == 'serie' else episodianime(item)
+
+    if config.get_library_support() and len(itemlist) != 0:
+        itemlist.append(
+            Item(channel=__channel__,
+                 title=item.title,
+                 url=item.url,
+                 action="add_serie_to_library",
+                 extra="episodios" + "###" + item.extra,
+                 show=item.show))
+        itemlist.append(
+            Item(channel=__channel__,
+                 title="Scarica tutti gli episodi della serie",
+                 url=item.url,
+                 action="download_all_episodes",
+                 extra="episodios" + "###" + item.extra,
+                 show=item.show))
+
+
 def episodianime(item):
-    logger.info("anime sub ita - episodianime")
+    logger.info("streamondemand.filmpertutti episodianime")
 
     itemlist = []
 
@@ -312,34 +344,18 @@ def episodianime(item):
                 scrapedtitle) < 99 and 'EASYBYTEZ' not in scrapedtitle.upper() and 'CLOUDSIX' not in scrapedtitle.upper():
             itemlist.append(
                 Item(channel=__channel__,
-                     action="findvid_serie",
+                     action="findvideos",
                      title="[COLOR azure]%s[/COLOR]" % scrapedtitle,
-                     url=item.url,
+                     url=data,
                      thumbnail=item.thumbnail,
-                     extra=data,
-                     fulltitle=item.fulltitle,
+                     extra=item.extra,
+                     fulltitle=item.show + ' | ' + item.fulltitle,
                      show=item.show))
-
-    if config.get_library_support() and len(itemlist) != 0:
-        itemlist.append(
-            Item(channel=__channel__,
-                 title=item.title,
-                 url=item.url,
-                 action="add_serie_to_library",
-                 extra="episodios",
-                 show=item.show))
-        itemlist.append(
-            Item(channel=__channel__,
-                 title="Scarica tutti gli episodi della serie",
-                 url=item.url,
-                 action="download_all_episodes",
-                 extra="episodios",
-                 show=item.show))
 
     return itemlist
 
 
-def episodios(item):
+def episodiserie(item):
     def load_episodios(html, item, itemlist, lang_title):
         patron = '((?:.*?<a href="[^"]+" target="_blank" rel="nofollow">[^<]+</a>)+)'
         matches = re.compile(patron).findall(html)
@@ -349,15 +365,15 @@ def episodios(item):
             scrapedtitle = re.sub(r'<[^>]*>', '', scrapedtitle).strip()
             itemlist.append(
                 Item(channel=__channel__,
-                     action="findvid_serie",
+                     action="findvideos",
                      title="[COLOR azure]%s[/COLOR]" % (scrapedtitle + " (" + lang_title + ")"),
-                     url=item.url,
+                     url=data,
                      thumbnail=item.thumbnail,
-                     extra=data,
-                     fulltitle=item.fulltitle,
+                     extra=item.extra,
+                     fulltitle=item.show + ' | ' + item.fulltitle,
                      show=item.show))
 
-    logger.info("streamondemand.filmpertutti episodios")
+    logger.info("streamondemand.filmpertutti episodiserie")
 
     itemlist = []
 
@@ -389,30 +405,14 @@ def episodios(item):
 
         i += 1
 
-    if config.get_library_support() and len(itemlist) != 0:
-        itemlist.append(
-            Item(channel=__channel__,
-                 title=item.title,
-                 url=item.url,
-                 action="add_serie_to_library",
-                 extra="episodios",
-                 show=item.show))
-        itemlist.append(
-            Item(channel=__channel__,
-                 title="Scarica tutti gli episodi della serie",
-                 url=item.url,
-                 action="download_all_episodes",
-                 extra="episodios",
-                 show=item.show))
-
     return itemlist
 
 
-def findvid_serie(item):
+def findvideos(item):
     logger.info("streamondemand.filmpertutti findvideos")
 
     # Descarga la página
-    data = item.extra
+    data = scrapertools.cache_page(item.url) if item.extra == 'film' else item.url
 
     itemlist = servertools.find_video_items(data=data)
 
